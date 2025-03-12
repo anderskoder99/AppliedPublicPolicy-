ab0072654c9693c3aad0f6facf00f1dc44aa826a
anderskoder99 <andershansen1999@yahoo.com>
2025-03-12 14:42:53 +0100

    Test af github

--- Script.R
+++ Script.R
@@ -1,4 +1,8 @@
 # 1. Indlæs pakker -----------------------------------------------------------
+install.packages("readxl") #Installerer pakken readxl
+install.packages("here") #Installerer pakken here
+install.packages("tidyverse") #Installerer pakken tidyverse
+
 library(readxl) #Bruges til at indlæse data i excel-form
 library(here) #Bruges til at skabe en relative path til data
 library(tidyverse) 
@@ -12,6 +16,18 @@ View(data) #Viser data i R-studio for at tjekke at det blev loaded korrekt
 
 # 3. Datarensning -----------------------------------------------------------
 glimpse(data) #Viser dataens struktur 
+
+unique(data$Udrulning_DW)
+sum(data$Udrulning_DW == "Udenfor distrikt") #580 rækker har denne værdi
+data <- data[data$Udrulning_DW != "Udenfor distrikt", ] #Fjerner rækker med denne værdi
+
+
+
+#Datatype
+#Tjek udfaldsvariables udfaldsmuligheder
+#summary  
+#Fjern missing variables - 
+#Evt. ændre "NULL" til "0" 
 
 
 
