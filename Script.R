0f065def159e3c9a87fe4de079ed35ac32041ac8
anderskoder99 <andershansen1999@yahoo.com>
2025-03-12 14:04:30 +0100

    Descriptive message about the changes

--- Script.R
+++ Script.R
@@ -1,1 +1,15 @@
-#Test
+# 1. Indlæs pakker -----------------------------------------------------------
+library(readxl) #Bruges til at indlæse data i excel-form
+library(here) #Bruges til at skabe en relative path til data
+library(tidyverse) 
+
+data <- read_excel(here("Data", "RV data_anonymiseret 25feb2025.xlsx"))
+
+# 2. Indlæs data -------------------------------------------------------------
+
+#Sikre at alle brugere har samme path til data
+data <- read_excel(here("Data", "RV data_anonymiseret 25feb2025.xlsx"))
+
+View(data) #Viser data i R-studio for at tjekke at det blev loaded korrekt
+
+# 3. Datarensning -----------------------------------------------------------
