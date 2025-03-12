8fabbf336ece358b9f3a241ff50c13af7dde8138
anderskoder99 <andershansen1999@yahoo.com>
2025-03-12 14:06:01 +0100

    Beskrivelse af ændringer

--- Script.R
+++ Script.R
@@ -3,8 +3,6 @@ library(readxl) #Bruges til at indlæse data i excel-form
 library(here) #Bruges til at skabe en relative path til data
 library(tidyverse) 
 
-data <- read_excel(here("Data", "RV data_anonymiseret 25feb2025.xlsx"))
-
 # 2. Indlæs data -------------------------------------------------------------
 
 #Sikre at alle brugere har samme path til data
@@ -13,3 +11,8 @@ data <- read_excel(here("Data", "RV data_anonymiseret 25feb2025.xlsx"))
 View(data) #Viser data i R-studio for at tjekke at det blev loaded korrekt
 
 # 3. Datarensning -----------------------------------------------------------
+glimpse(data) #Viser dataens struktur 
+
+
+
+
