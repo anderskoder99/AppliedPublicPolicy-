b84b87e304ba2cd4140cb66f56b372134fb76907
sophiakallestrup <budzeen@live.dk>
2025-03-29 08:17:58 +0100

    Update Script.R

--- Script.R
+++ Script.R
@@ -39,6 +39,8 @@ unique(data$Ydelse) #OBS på denne: 260 unikke værdier - skal vi gruppere?
 data$Fraværsprocent[data$Fraværsprocent == "NULL"] <- NA
 data$AntalFraværsdage[data$AntalFraværsdage == "NULL"] <- NA
 data$AntalSkoledage[data$AntalSkoledage == "NULL"] <- NA
+data$AntalSkoledage[data$YdelseDatoStart == "NULL"] <- NA
+data$AntalSkoledage[data$YdelseDatoStop == "NULL"] <- NA
 
 # 3.2 Korrekt variabeltype ------------------------------------------------
 glimpse(data$Halvår_DW) #Viser samtlige variables type. 
@@ -53,16 +55,88 @@ data <- data %>%
 
 
 # 4.1 Plot udvikling på udfaldsvariable: fraværsprocent, underretnigner (anntal + farvekoder) og ydelsesgruppering + ydelseslængde?  -----------------------------------
+# Lave Dato variable til dato format
+str(data$DatoFraJoin_DW)
+data$DatoFraJoin_DW <- as.Date(data$DatoFraJoin_DW)
+str(data$DatoTilJoin_DW)
+data$DatoTilJoin_DW <- as.Date(data$DatoTilJoin_DW)
 
-data |> 
-  group_by(Udrulning_DW) |> 
-  summarise(gns_fravær = mean(Fraværsprocent, na.rm = TRUE)) |> 
-  ggplot(aes(x = Halvår_DW, y = gns_fravær, group = Udrulning_DW)) + 
-  geom_point(na.rm = TRUE) +
-  geom_line(na.rm = TRUE) + 
-  theme_minimal()
+data <- data %>%
+  mutate(
+    YdelseDatoStart = na_if(YdelseDatoStart, "NULL"),  # Konverter "NULL" til NA
+    YdelseDatoStart = as.Date(YdelseDatoStart, format = "%m/%d/%Y")  # Konverter til dato
+  )
+
+data <- data %>%
+  mutate(
+    YdelseDatoStop = na_if(YdelseDatoStop, "NULL"),  # Konverter "NULL" til NA
+    YdelseDatoStop = as.Date(YdelseDatoStop, format = "%m/%d/%Y")  # Konverter til dato
+  )
+
+
+# Lav variabel for længde af ydelse 
+data$ydelseslængde_dage <- as.Date(data$YdelseDatoStop) - as.Date(data$YdelseDatoStart)
+
 
 str(data$Fraværsprocent)
+
+# Fravær
+data |> 
+  group_by(Udrulning_DW, DatoFraJoin_DW) |>  
+  summarise(gns_fravær = mean(Fraværsprocent, na.rm = TRUE), .groups = "drop") |> 
+  ggplot(aes(x = DatoFraJoin_DW, y = gns_fravær, group = Udrulning_DW, color = Udrulning_DW)) + 
+  geom_point(na.rm = TRUE) +
+  geom_line(na.rm = TRUE) + 
+  geom_vline(xintercept = as.numeric(as.Date(c("2023-02-01", "2023-09-01", "2024-02-01", "2024-09-01", "2025-02-01"))), 
+             linetype = "dashed", color = "black") +
+  theme_minimal()
+
+# Underretninger
+data |> 
+  group_by(Udrulning_DW, DatoFraJoin_DW) |>  
+  summarise(gns_underretning = mean(Underretning_DW, na.rm = TRUE), .groups = "drop") |> 
+  ggplot(aes(x = DatoFraJoin_DW, y = gns_underretning, group = Udrulning_DW, color = Udrulning_DW)) + 
+  geom_point(na.rm = TRUE) +
+  geom_line(na.rm = TRUE) + 
+  geom_vline(xintercept = as.numeric(as.Date(c("2023-02-01", "2023-09-01", "2024-02-01", "2024-09-01", "2025-02-01"))), 
+             linetype = "dashed", color = "black") +
+  theme_minimal()
+
+# Ydelseslængde
+data |> 
+  group_by(Udrulning_DW, DatoFraJoin_DW) |>  
+  summarise(gns_længde = mean(ydelseslængde_dage, na.rm = TRUE), .groups = "drop") |> 
+  ggplot(aes(x = DatoFraJoin_DW, y = gns_længde, group = Udrulning_DW, color = Udrulning_DW)) + 
+  geom_point(na.rm = TRUE) +
+  geom_line(na.rm = TRUE) + 
+  geom_vline(xintercept = as.numeric(as.Date(c("2023-02-01", "2023-09-01", "2024-02-01", "2024-09-01", "2025-02-01"))), 
+             linetype = "dashed", color = "black") +
+  theme_minimal()
+
+# Farvekode
+data %>%
+  filter(Farvekode_DW != "NULL") %>%  # Fjerner "NULL"
+  group_by(DatoFraJoin_DW, Farvekode_DW) %>%  # Gruppér efter dato og farve
+  summarise(antal_sager = n(), .groups = "drop") %>%  # Tæl antal sager
+  ggplot(aes(x = DatoFraJoin_DW, y = antal_sager, color = Farvekode_DW, group = Farvekode_DW)) + 
+  geom_point() + 
+  geom_line() + 
+  geom_vline(xintercept = as.numeric(as.Date(c("2023-02-01", "2023-09-01", "2024-02-01", "2024-09-01", "2025-02-01"))), 
+             linetype = "dashed", color = "black") +
+  theme_minimal()
+
+# Ydelsesgruppering
+data %>%
+  filter(Ydelsesgruppering_DW != "NULL") %>%  # Fjerner "NULL"
+  group_by(DatoFraJoin_DW, Ydelsesgruppering_DW) %>%  # Gruppér efter dato og farve
+  summarise(antal_sager = n(), .groups = "drop") %>%  # Tæl antal sager
+  ggplot(aes(x = DatoFraJoin_DW, y = antal_sager, color = Ydelsesgruppering_DW, group = Ydelsesgruppering_DW)) + 
+  geom_point() + 
+  geom_line() + 
+  geom_vline(xintercept = as.numeric(as.Date(c("2023-02-01", "2023-09-01", "2024-02-01", "2024-09-01", "2025-02-01"))), 
+             linetype = "dashed", color = "black") +
+  theme_minimal()
+unique(data$Ydelsesgruppering_DW)
 
 #forsøg______________________________________________
 #Lav treatment variabel 
